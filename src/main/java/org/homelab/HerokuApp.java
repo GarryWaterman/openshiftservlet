516af89a8e65d7d894fd444f3f9b3d2172c18ba9
GW <watermangarry@gmail.com>
2018-01-29 13:40:55 +0400

    1

---
+++ src/main/java/org/homelab/HerokuApp.java
@@ -0,0 +1,8 @@
+package org.homelab;
+
+public class HerokuApp {
+    public static void main(String[] args) {
+        // Do required initialization
+        System.out.println("main section started");
+    }
+}
